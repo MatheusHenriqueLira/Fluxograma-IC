fb77f169ef5585bdaa9ac436575e0bad7c19ae7b
Matheus Henrique Lira <Matheus2007Lira@gmail.com>
2026-08-20 18:32:09 -0300

    commit antes de uma mudança perigosa...

--- src/componentes/blocos/node.jsx
+++ src/componentes/blocos/node.jsx
@@ -1,12 +1,22 @@
 import { Handle, Position } from "@xyflow/react";
 
-function DefaultNode({ data }) {
+function DefaultNode({ id, data }) {
 
     return (
 
         <div className="defaultNode">
             <Handle type="target" position={Position.Top} className="node-handle" />
             <span>{data?.label ?? "Processo"}</span>
+            <input
+                type="text"
+                placeholder="Expressão"
+                value={data?.expression ?? ""}
+                onChange={(e) =>
+                    data.atualizarNode(id, {
+                        expression: e.target.value
+                    })
+                }
+            />
             <Handle type="source" position={Position.Bottom} className="node-handle" />
         </div>
 
