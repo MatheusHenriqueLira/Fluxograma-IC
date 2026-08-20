ae152b147801e11ff7decd1b2f0e83cf18154391
Matheus Henrique Lira <Matheus2007Lira@gmail.com>
2026-08-20 20:42:43 -0300

    Input saida

--- src/componentes/blocos/saida.jsx
+++ src/componentes/blocos/saida.jsx
@@ -1,12 +1,22 @@
 import { Handle, Position } from "@xyflow/react";
 
-function SaidaNode({ data }) {
+function SaidaNode({ id, data }) {
 
     return (
 
         <div className="saidaNode">
             <Handle type="target" position={Position.Top} className="node-handle" />
             <span>{data?.label ?? "Saída"}</span>
+            <input
+                type="text"
+                placeholder="Expressão"
+                value={data?.expression ?? ""}
+                onChange={(e) =>
+                    data.atualizarNode(id, {
+                        expression: e.target.value
+                    })
+                }
+            />
             <Handle type="source" position={Position.Bottom} className="node-handle" />
         </div>
 
