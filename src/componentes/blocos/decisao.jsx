399fe04bf0f1c31fd90986a5dff9dafcb5d1ebb5
Matheus Henrique Lira <Matheus2007Lira@gmail.com>
2026-08-20 19:34:55 -0300

    Input decisao

--- src/componentes/blocos/decisao.jsx
+++ src/componentes/blocos/decisao.jsx
@@ -1,24 +1,36 @@
 import { Handle, Position } from "@xyflow/react";
 
-function DecisionNode({ data }){
+function DecisionNode({id, data }) {
 
-    return(
+    return (
 
         <div className="decisionNode">
             <Handle type="target" position={Position.Top} className="node-handle" />
-            <div className="gambiarra">
-                <span>{data?.label ?? "Decisão"}</span>
+            <div className="decisionDiamond">
+                <div className="decisionContent">
+                    <span>{data?.label ?? "Decisão"}</span>
+                <input
+                    type="text"
+                    placeholder="Condição"
+                    value={data?.condition ?? ""}
+                    onChange={(e) =>
+                        data.atualizarNode(id, {
+                            condition: e.target.value
+                        })
+                    }
+                />
+                </div>
             </div>
             <Handle type="source"
-            position={Position.Left} 
-            className="node-handle"
-            id="left"
+                position={Position.Left}
+                className="node-handle"
+                id="left"
             />
             <Handle type="source"
-            position={Position.Right}
-            className="node-handle"
-            id="right"
-             />
+                position={Position.Right}
+                className="node-handle"
+                id="right"
+            />
         </div>
 
     )
